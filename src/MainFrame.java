8dedef104ef69382707a869a4cbfcb6b2f3accc3
Chris Newsome <legoclonetrooperdude@gmail.com>
2014-11-10 10:00:44 -0800

    mashed potate

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,11 +1,27 @@
-import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 
-public class MainFrame extends JFrame {
-//ladybugs smell nice
-super("Card Randomizer");
+public class MainFrame extends JPanel 
+{
+	
+
+
 public MainFrame()
 {
 	
 }
+
+public static void main(String[] args)
+{
+	String answer = JOptionPane.showInputDialog(null, "can i receive some mashed potate");
+	String answer2 = null;
+	if(answer.equals("yes"))
+		answer2 = JOptionPane.showInputDialog(null, "and do you believe in the separation of church and hate");
+	if(answer2.equals("yes"))
+		JOptionPane.showMessageDialog(null, "u a troo playa");
+	
+	
+}
+
 }
