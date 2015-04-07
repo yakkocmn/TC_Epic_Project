2b56deb9a272e8787b6c11c4a703a64de6b2001f
unknown <gsullivan2016@02925pc.francisparker.org>
2015-04-07 12:26:46 -0700

    distinct shuffles in progress

--- src/Deck.java
+++ src/Deck.java
@@ -5,113 +5,111 @@ import java.util.Random;
 
 
 public class Deck {
-	
+
 	private ArrayList<Card> cards;
 	private ArrayList<Card> removedCards;
-	
+
 	public Deck()
 	{
 		cards = new ArrayList<Card>();
 		removedCards = new ArrayList<Card>();
-		
+
 		for(int i = 1; i <= 13; i++)
 		{
 			Card currCard = new Card(i, "club", Color.BLACK);
 			cards.add(currCard);
 		}
-		
+
 		for(int i = 1; i <= 13; i++)
 		{
 			Card currCard = new Card(i, "spade", Color.BLACK);
 			cards.add(currCard);
 		}
-		
+
 		for(int i = 1; i <= 13; i++)
 		{
 			Card currCard = new Card(i, "heart", Color.RED);
 			cards.add(currCard);
 		}
-		
+
 		for(int i = 1; i <= 13; i++)
 		{
 			Card currCard = new Card(i, "diamond", Color.RED);
 			cards.add(currCard);
 		}
 		//System.out.println(this.getNumberOfCards());
-		
+
 	}
-	
+
 	public void shuffle()
 	{
-//		cards.addAll(removedCards);
-//		removedCards.clear();
-//		 Collections.shuffle(cards);
-		
-		ArrayList<Card> empty = new ArrayList<Card>();
-		for(int i=0;cards.size()<0 && i<52;i++)
-		{
-			
-		double random = (Math.random()*cards.size()+1);
-		int val = (int)random;
-		empty.add(cards.get(val));
-		cards.remove(val);
-		}
-		
-		cards=empty;
-		empty.clear();
-		cards.clear();
-		for(int i = 1; i <= 13; i++)
-		{
-			Card currCard = new Card(i, "club", Color.BLACK);
-			cards.add(currCard);
-		}
-		
-		for(int i = 1; i <= 13; i++)
-		{
-			Card currCard = new Card(i, "spade", Color.BLACK);
-			cards.add(currCard);
-		}
-		
-		for(int i = 1; i <= 13; i++)
-		{
-			Card currCard = new Card(i, "heart", Color.RED);
-			cards.add(currCard);
-		}
-		
-		for(int i = 1; i <= 13; i++)
-		{
-			Card currCard = new Card(i, "diamond", Color.RED);
-			cards.add(currCard);
-		}
-		 if(getNumberOfCards()==52)
-			 System.out.println("shuffle successful");
-		 else
-			 System.out.println("shuffle NOT successful");
-	        
-	}
+		//		cards.addAll(removedCards);
+		//		removedCards.clear();
+		Collections.shuffle(cards);
+		//		
+		//		ArrayList<Card> empty = new ArrayList<Card>();
+		//		for(int i=0;cards.size()<0 && i<52;i++)
+		//		{
+		//			
+		//		double random = (Math.random()*cards.size()+1);
+		//		int val = (int)random;
+		//		empty.add(cards.get(val));
+		//		cards.remove(val);
+		//		}
+		//		
+		//		empty.clear();
+		//		empty=cards;
+		//		cards.clear();
+		//		while(cards.size()<empty.size())
+		//		{
+		//			int x = Math.random()empty
+
+
+
+	if(getNumberOfCards()==cards.size())
+		System.out.println("shuffle successful");
+
+
+}
 	
-	public Card getCard(int place)
+	private void fshuffle()
 	{
-		Card desiredCard = cards.get(place);
-		cards.remove(place);
-		removedCards.add(desiredCard);
-		return desiredCard;
-	}
-	
-	public Card getCard(Card card)
+	while(cards.size()<52)
 	{
-		Card desiredCard = card;
-		cards.remove(card);
-		removedCards.add(desiredCard);
-		return desiredCard;
+		//PUT REMOVED CARDS, 1 BY 1 IN CARDS
+		
 	}
 	
-	public int getNumberOfCards()
-	{
-		return cards.size();
+	if(getNumberOfCards()==52)
+		System.out.println("shuffle successful");
+	else
+		System.out.println("shuffle NOT successful");
+	//THEN NEED TO STOP DISPLAYING PICTURE
+	
 	}
-	
-	//public void 
-	
+
+public Card getCard(int place)
+{
+	Card desiredCard = cards.get(place);
+	cards.remove(place);
+	removedCards.add(desiredCard);
+	return desiredCard;
+}
+
+public Card getCard(Card card)
+{
+	Card desiredCard = card;
+	cards.remove(card);
+	removedCards.add(desiredCard);
+	return desiredCard;
+}
+
+public int getNumberOfCards()
+{
+	return cards.size();
+}
+
+//public void 
+
 
 }
